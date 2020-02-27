0995b4f18d29641797e949790f3e321e9ec8d20b
shwin0901 <401815708@qq.com>
2020-02-27 19:27:15 +0800

    add extra page

---
+++ src/components/extra/Privacy policy.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <titleDiv></titleDiv>
+    <div></div>
+    <footerDiv></footerDiv>
+  </div>
+</template>
+
+<script>
+import titleDiv from '../Index/Title';
+import footerDiv from '../Index/Footer'
+
+export default {
+  name:'privacy',
+  components: {
+    titleDiv,
+    footerDiv
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+  *{
+    padding: 0;
+    margin: 0;
+    text-align: center;
+  }
+</style>
+
+
+
+
+
+
+
+
---
+++ src/components/extra/Terms of service.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <titleDiv></titleDiv>
+    <div></div>
+    <footerDiv></footerDiv>
+  </div>
+</template>
+
+<script>
+import titleDiv from '../Index/Title';
+import footerDiv from '../Index/Footer'
+
+export default {
+  name:'terms',
+  components:{
+    titleDiv,
+    footerDiv
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  *{
+    padding:0;
+    margin: 0;
+    text-align: center;
+  }
+</style>
